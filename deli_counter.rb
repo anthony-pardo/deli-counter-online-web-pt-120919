3bcb29301e2c002ceeaf8b85c97d1d9639e9bb55
Anthony Pardo <pardo.anthony@gmail.com>
2019-12-12 16:50:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,13 @@
 def line(arr)
   str = "The line is currently: "
+  new_arr = []
   if arr.size == 0 
     puts "The line is currently empty."
   else 
     arr.each_with_index do |name, index| 
-      
+      new_arr << "#{index + 1}. #{name}"
     end
+    new_str = new_arr.join(" ")
+    puts str + new_str
   end
 end
